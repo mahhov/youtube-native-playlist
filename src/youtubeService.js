b20ba3105d77ebdea6381106dda0209680649224
m <mahhov1@gmail.com>
2018-04-03 17:55:30 -0700

    clean async stream to fetch playlsit videos

--- src/youtubeService.js
+++ src/youtubeService.js
@@ -4,22 +4,18 @@ const apiUrl = 'https://www.googleapis.com/youtube/v3';
 const apiKey = 'AIzaSyAdkXuGc2f7xJg5FLTWBi2cRUhzAJD-eC0';
 
 let streamPlaylistVideos = id => {
-    let videos = _();
-    streamPlaylistPage(videos, id, '');
-    return videos.map(video => ({id: video.snippet.resourceId.videoId, title: video.snippet.title}));
-};
-
-let streamPlaylistPage = async (videos, id) => {
-    let nextPage = '';
-    do {
-        let playlistPage = await getPlaylistPage(id, nextPage);
-        videos.write(...playlistPage.items);
-        nextPage = playlistPage.nextPageToken;
-    } while (nextPage);
+    let pages = _();
+    let responses = pages
+        .map(page => getPlaylistPage(id, page))
+        .wait();
+    responses.pluck('nextPageToken').filter(nextPage => nextPage).to(pages);
+    let snippets = responses.pluck('items').flatten().pluck('snippet');
+    pages.write('');
+    return snippets;
 };
 
 let getPlaylistPage = (id, page) =>
-    axios.get(`${apiUrl}/playlistItems?part=snippet&maxResults=4&pageToken=${page}&playlistId=${id}&key=${apiKey}`)
+    axios.get(`${apiUrl}/playlistItems?part=snippet&maxResults=50&pageToken=${page}&playlistId=${id}&key=${apiKey}`)
         .then(response => response.data);
 
 module.exports = {streamPlaylistVideos};
