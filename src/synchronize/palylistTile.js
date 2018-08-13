9b41ea72b9ce301d3b0635bb90564f74c1b8e29e
mahhov <mahhov1@gmail.com>
2018-08-13 16:07:37 -0700

    removed onload experiment wip thing

--- src/synchronize/palylistTile.js
+++ src/synchronize/palylistTile.js
@@ -1,8 +1,6 @@
 let template = require('fs').readFileSync(`${__dirname}/playlistTile.html`, 'utf8');
 
 let controller = source => {
-	source.onload = playlist =>
-		console.log('loaded', playlist, source.playlist);
 };
 
 let parameters = ['playlist'];
