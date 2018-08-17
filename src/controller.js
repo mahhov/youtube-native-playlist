c0e80576898467822bd19fa1cd92c40a75378242
mahhov <mahhov1@gmail.com>
2018-08-17 16:33:50 -0700

    extracted synchronize block

--- src/controller.js
+++ src/controller.js
@@ -1,37 +1,14 @@
-const storage = require('./backend/storage/storageService');
-const Playlist = require('./backend/youtube/Playlist');
-
 const bb = require('bb-better-binding')();
 bb.declareBlock('tab', require('./tab/tab'));
+bb.declareBlock('synchronize', require('./synchronize/controller'));
 bb.declareBlock('playlistTile', require('./synchronize/palylistTile'));
 const source = bb.boot(document.firstElementChild, window);
-
-// tabs
 
 source.setSelectedTab = tab =>
 	source.selectedTab = tab;
 
-// synchronize tab
-
-source.refreshPlaylistIds = () => {
-	source.playlists = storage.getPlaylistIds()
-		.map(id => new Playlist(id));
-};
-
-source.addPlaylistId = () => {
-	let id = source.playlistIdInput.value;
-	if (storage.addPlaylistId(id))
-		source.playlists.push(new Playlist(id));
-};
-
-source.removePlaylistId = id => {
-	if (storage.removePlaylistId(id))
-		source.playlists = source.playlists.filter(playlist => playlist.id !== id);
-};
-
 let init = () => {
 	source.setSelectedTab('synchronize');
-	source.refreshPlaylistIds();
 };
 
 init();
---
+++ src/synchronize/controller.js
@@ -0,0 +1,30 @@
+const template = require('fs').readFileSync(`${__dirname}/index.html`, 'utf8');
+const storage = require('../backend/storage/storageService');
+const Playlist = require('../backend/youtube/Playlist');
+
+let controller = source => {
+	source.refreshPlaylistIds = () => {
+		source.playlists = storage.getPlaylistIds()
+			.map(id => new Playlist(id));
+	};
+
+	source.addPlaylistId = () => {
+		let id = source.playlistIdInput.value;
+		if (storage.addPlaylistId(id))
+			source.playlists.push(new Playlist(id));
+	};
+
+	source.removePlaylistId = id => {
+		if (storage.removePlaylistId(id))
+			source.playlists = source.playlists.filter(playlist => playlist.id !== id);
+	};
+
+
+	let init = () => source.refreshPlaylistIds();
+
+	init();
+};
+
+let parameters = [];
+
+module.exports = {template, controller, parameters};
